08167c5a34f4f5399d77869b9ed8306e8772bf2f
kienND9 <KienND9@fsoft.com.vn>
2017-06-19 16:58:44 +0700

    + Add Home Screen

---
+++ KNCinema/ViewController/HomeViewController/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  KNCinema
+//
+//  Created by kienND9 on 6/19/17.
+//  Copyright © 2017 ABC. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
